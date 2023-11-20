c4f988c3e389575d0435d29d529026f8b640cba7
User <ckananelo4@gmail.com>
2023-11-20 11:46:15 +0200

    first commit

---
+++ java.js
@@ -0,0 +1,37 @@
+var Rating = [1,2,3,4,5]
+
+var Sum = 0
+
+function Rating(){
+    
+    document.getElementById("Rate").innerHTML = "You selected " + Rating[0]
+        
+}
+
+function Rating(){
+    
+    document.getElementById("Rate").innerHTML = "You selected " + Rating[1]
+        
+}
+
+function Rating(){
+    
+    document.getElementById("Rate").innerHTML = "You selected " + Rating[2]
+        
+}
+
+function Rating(){
+    
+    document.getElementById("Rate").innerHTML = "You selected " + Rating[3]
+        
+}
+
+function Rating(){
+    
+    document.getElementById("Rate").innerHTML = "You selected " + Rating[4]
+        
+}    
+    
+    
+    
+    
