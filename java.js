1e7eda94593fd1fe9027f08ef65494a40112b8ae
User <ckananelo4@gmail.com>
2023-11-23 15:57:17 +0200

    new changes

--- java.js
+++ java.js
@@ -1,37 +1,65 @@
 var Rating = [1,2,3,4,5]
 
-var Sum = 0
 
-function Rating(){
+function Rating1(){
     
-    document.getElementById("Rate").innerHTML = "You selected " + Rating[0]
+    document.getElementById("Rate").innerHTML = "You selected " + Rating[0] + " out of 5"
+    document.getElementById("b1").style.backgroundColor = "#7c8898"
+    document.getElementById("b2").style.backgroundColor = "rgb(56, 56, 68)"
+    document.getElementById("b3").style.backgroundColor = "rgb(56, 56, 68)"
+    document.getElementById("b4").style.backgroundColor = "rgb(56, 56, 68)"
+    document.getElementById("b5").style.backgroundColor = "rgb(56, 56, 68)"
+    
+}
+
+function Rating2(){
+    
+    document.getElementById("Rate").innerHTML = "You selected " + Rating[1] + " out of 5"
+    document.getElementById("b1").style.backgroundColor = "rgb(56, 56, 68)"
+    document.getElementById("b2").style.backgroundColor = "#7c8898"
+    document.getElementById("b3").style.backgroundColor = "rgb(56, 56, 68)"
+    document.getElementById("b4").style.backgroundColor = "rgb(56, 56, 68)"
+    document.getElementById("b5").style.backgroundColor = "rgb(56, 56, 68)"
         
 }
 
-function Rating(){
+function Rating3(){
     
-    document.getElementById("Rate").innerHTML = "You selected " + Rating[1]
+    document.getElementById("Rate").innerHTML = "You selected " + Rating[2] + " out of 5"
+    document.getElementById("b1").style.backgroundColor = "rgb(56, 56, 68)"
+    document.getElementById("b2").style.backgroundColor = "rgb(56, 56, 68)"
+    document.getElementById("b3").style.backgroundColor = "#7c8898"
+    document.getElementById("b4").style.backgroundColor = "rgb(56, 56, 68)"
+    document.getElementById("b5").style.backgroundColor = "rgb(56, 56, 68)"
         
 }
 
-function Rating(){
+function Rating4(){
     
-    document.getElementById("Rate").innerHTML = "You selected " + Rating[2]
+    document.getElementById("Rate").innerHTML = "You selected " + Rating[3] + " out of 5"
+    document.getElementById("b1").style.backgroundColor = "rgb(56, 56, 68)"
+    document.getElementById("b2").style.backgroundColor = "rgb(56, 56, 68)"
+    document.getElementById("b3").style.backgroundColor = "rgb(56, 56, 68)"
+    document.getElementById("b4").style.backgroundColor = "#7c8898"
+    document.getElementById("b5").style.backgroundColor = "rgb(56, 56, 68)"
         
 }
 
-function Rating(){
+function Rating5(){
     
-    document.getElementById("Rate").innerHTML = "You selected " + Rating[3]
-        
-}
-
-function Rating(){
-    
-    document.getElementById("Rate").innerHTML = "You selected " + Rating[4]
+    document.getElementById("Rate").innerHTML = "You selected " + Rating[4] + " out of 5"
+    document.getElementById("b1").style.backgroundColor = "rgb(56, 56, 68)"
+    document.getElementById("b2").style.backgroundColor = "rgb(56, 56, 68)"
+    document.getElementById("b3").style.backgroundColor = "rgb(56, 56, 68)"
+    document.getElementById("b4").style.backgroundColor = "rgb(56, 56, 68)"
+    document.getElementById("b5").style.backgroundColor = "#7c8898"
         
 }    
     
-    
-    
-    
+function Submit(){
+
+document.getElementById("box").style.display = "none"
+document.getElementById("box2").style.display = "block"
+
+}
+
